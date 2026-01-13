99e853a780fd8c5e82c81528deb2bf2c5ba68a29
Jelle Vansteenkiste <jelle.vansteenkiste@student.vives.be>
2026-01-13 18:38:18 +0100

    changed procuctListScreen

--- Jelle_Vansteenkiste_examen_MiniShop/src/screens/ProductListScreen.tsx
+++ Jelle_Vansteenkiste_examen_MiniShop/src/screens/ProductListScreen.tsx
@@ -1,18 +1,34 @@
+import { View, Text, FlatList, ActivityIndicator } from 'react-native';
+import { useQuery } from '@tanstack/react-query';
+import { fetchProducts } from '../api/products';
+import ProductCard from '../components/ProductCard';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { HomeStackParamList } from '../navigation/types/HomeStackParamList';
-import { View, Button } from 'react-native';
+import { HomeStackParamList } from '../navigation/HomeStack';
 
 type Props = NativeStackScreenProps<HomeStackParamList, 'ProductList'>;
 
 export default function ProductListScreen({ navigation }: Props) {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['products'],
+    queryFn: fetchProducts,
+  });
+
+  if (isLoading) return <ActivityIndicator style={{ marginTop: 50 }} />;
+  if (isError) return <Text>❌ Error loading products</Text>;
+  if (!data.products.length) return <Text>Geen producten</Text>;
+
   return (
-    <View>
-      <Button
-        title="Ga naar product"
-        onPress={() =>
-          navigation.navigate('ProductDetail', { productId: '123' })
-        }
-      />
-    </View>
+    <FlatList
+      data={data.products}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item }) => (
+        <ProductCard
+          product={item}
+          onPress={() =>
+            navigation.navigate('ProductDetail', { id: item.id })
+          }
+        />
+      )}
+    />
   );
 }
