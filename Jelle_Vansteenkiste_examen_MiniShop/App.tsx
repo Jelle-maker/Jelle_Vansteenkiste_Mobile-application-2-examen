58e6eaa2528da70bb071d4bbdbf14ebd9cb623fb
Jelle Vansteenkiste <jelle.vansteenkiste@student.vives.be>
2026-01-13 18:49:46 +0100

    changed app.tsx corrected rooting

--- Jelle_Vansteenkiste_examen_MiniShop/App.tsx
+++ Jelle_Vansteenkiste_examen_MiniShop/App.tsx
@@ -1,9 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { store } from './src/store/store';
-import RootTabs from './src/navigation/RootTabs';
-import { ThemeProvider } from './src/theme/ThemeContext';
+import { store } from './src/redux/Index';
+import RootTabs from './src/navigation/Tabs';
+import { ThemeProvider } from './src/theme/theme';
 
 const queryClient = new QueryClient();
 
