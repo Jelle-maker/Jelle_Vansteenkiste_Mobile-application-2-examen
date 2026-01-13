eae374a7536c3b570015bd3b677bc7ae2f043b39
Jelle Vansteenkiste <jelle.vansteenkiste@student.vives.be>
2026-01-13 18:43:08 +0100

    changed cartSlice

--- Jelle_Vansteenkiste_examen_MiniShop/src/redux/cartSlice.ts
+++ Jelle_Vansteenkiste_examen_MiniShop/src/redux/cartSlice.ts
@@ -1,28 +1,26 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-type CartItem = {
+export interface CartItem {
   id: number;
   title: string;
   price: number;
   quantity: number;
-};
+}
 
-type CartState = {
+interface CartState {
   items: CartItem[];
-};
+}
 
-const initialState: CartState = {
-  items: [],
-};
+const initialState: CartState = { items: [] };
 
 const cartSlice = createSlice({
-  name: "cart",
+  name: 'cart',
   initialState,
   reducers: {
-    addToCart(state, action: PayloadAction<Omit<CartItem, "quantity">>) {
+    addToCart(state, action: PayloadAction<CartItem>) {
       const item = state.items.find(i => i.id === action.payload.id);
-      if (item) item.quantity++;
-      else state.items.push({ ...action.payload, quantity: 1 });
+      if (item) item.quantity += 1;
+      else state.items.push(action.payload);
     },
     increment(state, action: PayloadAction<number>) {
       const item = state.items.find(i => i.id === action.payload);
@@ -32,13 +30,23 @@ const cartSlice = createSlice({
       const item = state.items.find(i => i.id === action.payload);
       if (item && item.quantity > 1) item.quantity--;
     },
-    removeItem(state, action: PayloadAction<number>) {
+    remove(state, action: PayloadAction<number>) {
       state.items = state.items.filter(i => i.id !== action.payload);
     },
   },
 });
 
-export const { addToCart, increment, decrement, removeItem } =
-  cartSlice.actions;
+export const {
+  addToCart,
+  increment,
+  decrement,
+  remove,
+} = cartSlice.actions;
+
+export const selectTotalItems = (state: { cart: CartState }) =>
+  state.cart.items.reduce((sum, i) => sum + i.quantity, 0);
+
+export const selectSubtotal = (state: { cart: CartState }) =>
+  state.cart.items.reduce((sum, i) => sum + i.price * i.quantity, 0);
 
 export default cartSlice.reducer;
